a968018ec998f5a38dfd46699ee7e2921f461a4e
Rongxin Zhang <rongxin@rets.ly>
2017-07-16 23:57:52 -0400

    WIP: Bin file

--- run.py
+++ run.py
@@ -2,7 +2,28 @@
 import sys
 import os
 
-from basic import *
+import basic.string1 as basicStr
+import basic.list1 as basicList
+import basic.wordcount as basicWordCount
+
+print('----------------------')
+print('-----EX 1: Basic------')
+print('----------------------')
+print()
+print('--------String--------')
+print()
+basicStr.main()
+print()
+print('---------List---------')
+print()
+basicList.main()
+print()
+print('-------WordCount------')
+print()
+
+
 from babynames import *
 from copyspecial import *
 from logpuzzle import *
+
+basicWordCount.main()
