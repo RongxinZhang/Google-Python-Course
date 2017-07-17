c5accc57a071efea25d8cc380ee82584b76c1d74
Rongxin Zhang <rongxin@rets.ly>
2017-07-16 22:07:40 -0400

    Ex Part4: Add file creation

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -15,13 +15,38 @@ import subprocess
 """Copy Special exercise
 """
 
+"""
+1. Get files in directory from which the code is run
+2. Check for files which contains "__<WORD>__"
+3. Store absolute path
+"""
 def get_special_paths(dir):
-    absPath = ""
-    
-    return absPath
+    allFiles = os.listdir(dir)
 
-def copy_to(paths, dir):
-    pass
+    matchedFiles = []
+
+    for i in allFiles:
+        matched = re.match('[w\]__[\w]+__\.\w', i)
+        if matched:
+            absPath = os.path.abspath(i)
+            matchedFiles.append(absPath)
+
+    return matchedFiles
+
+"""
+1. given a list of paths, copies those files into the given directory
+"""
+def copy_to(paths, toDir):
+
+    if not os.path.isdir(toDir):
+        try:
+          os.makedirs(toDir)
+        except OSError as e:
+          if e.errno != errno.EEXIST:
+              raise
+
+    # shutil.copy()
+
 
 def zip_to(paths, zippath):
     pass
@@ -30,32 +55,34 @@ def zip_to(paths, zippath):
 # Write functions and modify main() to call them
 
 def main():
+    # print(get_special_paths('./'))
+    copy_to([], './test')
   # This basic command line argument parsing code is provided.
   # Add code to call your functions below.
 
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
-  args = sys.argv[1:]
-  if not args:
-    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
-    sys.exit(1)
-
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
-
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
-
-  if len(args) == 0:
-    print "error: must specify one or more dirs"
-    sys.exit(1)
+  # args = sys.argv[1:]
+  # if not args:
+  #   print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+  #   sys.exit(1)
+  #
+  # # todir and tozip are either set from command line
+  # # or left as the empty string.
+  # # The args array is left just containing the dirs.
+  # todir = ''
+  # if args[0] == '--todir':
+  #   todir = args[1]
+  #   del args[0:2]
+  #
+  # tozip = ''
+  # if args[0] == '--tozip':
+  #   tozip = args[1]
+  #   del args[0:2]
+  #
+  # if len(args) == 0:
+  #   print "error: must specify one or more dirs"
+  #   sys.exit(1)
 
   # +++your code here+++
   # Call your functions
